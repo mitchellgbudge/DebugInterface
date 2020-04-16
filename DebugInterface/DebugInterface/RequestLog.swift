b7a7d6e4a6602d0edfc497a65c8805abcaee768c
Mitchell Budge <mitchellbudge@gmail.com>
2020-04-16 14:58:51 -0600

    Configure base master view template

---
+++ DebugInterface/DebugInterface/RequestLog.swift
@@ -0,0 +1,26 @@
+//
+//  RequestLog.swift
+//  DebugInterface
+//
+//  Created by Mitchell Budge on 4/16/20.
+//  Copyright © 2020 Mitchell Budge. All rights reserved.
+//
+
+import SwiftUI
+
+struct RequestLog : View {
+    var body: some View {
+        NavigationView {
+            Text("")
+            .navigationBarTitle("Requests")
+        }
+    }
+    
+    
+}
+
+struct RequestLog_Previews: PreviewProvider {
+    static var previews: some View {
+        RequestLog()
+    }
+}
