61f185ed9b2cffbf336893cf90a727afa8dac322
Mitchell Budge <mitchellbudge@gmail.com>
2020-04-16 14:54:18 -0600

    Add base project

---
+++ DebugInterface/DebugInterface/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DebugInterface
+//
+//  Created by Mitchell Budge on 4/16/20.
+//  Copyright © 2020 Mitchell Budge. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
